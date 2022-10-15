6b19c821eb84a672fd73272c09da3e1eef5b6ba0
Kasun <Kasun@mydoctor.lk>
2022-10-15 13:41:27 +0530

    Initial Commit

---
+++ BikeStation/BikeStation/BikeStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeStationApp.swift
+//  BikeStation
+//
+//  Created by Kasun Ranasinghe on 2022-10-15.
+//
+
+import SwiftUI
+
+@main
+struct BikeStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
